158807c5bf4b0fdf0b58cd3780f72054c0daeab0
crowl1 <47753606+crowl1@users.noreply.github.com>
2020-05-25 00:28:39 +0300

    частково додано dal.abstract

---
+++ DAL.Abstract/IGoodRepository.cs
@@ -0,0 +1,13 @@
+﻿using mvvc_wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpz3.DAL.Abstract
+{
+    public interface IGoodRepository : IGenericRepository<Good>
+    {
+    }
+}
